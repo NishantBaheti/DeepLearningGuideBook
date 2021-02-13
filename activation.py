4191bbc55ded95de2ef3e432406471c065d80fdd
Nishant Baheti <nishantbaheti.it19@gmail.com>
2021-02-13 16:22:56 +0530

    progress commit

--- activation.py
+++ activation.py
@@ -3,103 +3,155 @@
 Notes:
 
     1. Introducing non linearity to the network. Why?
-    2. According to me we need one parameter to compare all the nodes results after learning
-         and passing the value to upcoming nodes.
+    2. According to me we need one parameter to compare all the nodes results after learning and passing the value to upcoming nodes.
     3. To make sense of the data and a mapping for approximation.
-    4. Understand what is the impact of weights and biases changing value 
-        to the network/nodes.
+    4. Understand what is the impact of weights and biases changing value to the network/nodes.
+        If there is only linear fx then it can only fit linear data but if we have not linear data like a sine wave then it will fail to do so. 
+    5. If there is no activate function then the whole network will be similar to a one linear node.
         
-        If there is only linear fx then it can only fit linear data but if we have not linear data 
-        like a sine wave then it will fail to do so.
+            w.T(w.T *(w.T * x + b) + b) + b ... = output 
+            
+=====================================================================================================
+"""
+import numpy as np
 
+class Activation_Stepwise:
+    """Stepwise Activation Fx
 
-    5. If there is no activate function then the whole network will be similar to a one linear
-        node.
-        
-        w.T(w.T *(w.T * x + b) + b) + b ... = output 
-    6. stepwise activation fx 
+    Notes:
 
         * non granular 
         * only 0 and 1
-        * more info on 
+        
+    References:
+        None
+    """
 
-    7. sigmoid activation fx
+    def __init__(self):
+        """
+        """
+        self.output = None
+
+    def forward(self, inputs):
+        """Apply Stepwise to inputs
+
+        Args:
+            inputs (numpy.ndarray) : input matrix
+
+        """
+        pass
+
+
+class Activation_Sigmoid:
+    """Sigmoid Activation Fx
+
+    Notes:
+
+        f(x) = 1 / (1 + e^-x)
 
         * granular
         * between 0 and 1
-        * more info on  
         * Comparatively complex calcultaion
 
-    8. ReLU activation fx 
+    References:
+        None
+    """
+
+    def __init__(self):
+        """
+        """
+        self.output = None
+
+    def forward(self, inputs):
+        """Apply Sigmoid to input
+        
+        Args:
+            inputs (numpy.ndarray) : input matrix
+        
+        """
+        self.output = 1 / (1 + np.exp(-inputs))
+
+    def backward(self, dvalues):
+        """
+        """
+        pass
+
+
+class Activation_ReLU:
+    """ReLU Activation Fx
+
+    Notes:
+            
+        f(x) = 0  if x <= 0
+        f(x) = x  if x > 0
 
         * granular
         * between 0 to x
-        * more info on 
         * easy calculation 
-        * almost linear but rectified so less than zeros are not allowed.
-            so introducing slight non linearity makes it eligible for an
-            activation function but also inherently easy and fast calculation 
-            than sigmoid.
+        * almost linear but rectified so less than zeros are not allowed.so introducing slight non linearity makes it eligible for an activation function but also inherently easy and fast calculation than sigmoid.
     
-    9. Softmax acitvation fx
+    References:
+        None
+    """
 
-        * get more info from Andrew's video 
+    def __init__(self):
+        """
+        """
+        self.inputs = None
+        self.output = None
+        self.dinputs = None 
 
-"""
+    def forward(self, inputs):
+        """Apply ReLU to input
+        
+        Args:
+            inputs (numpy.ndarray) : input matrix
+        
+        """
+        self.inputs = inputs # save inputs 
+        self.output = np.maximum(0, inputs) # calculate from inputs
+
+    def backward(self, dvalues):
+        """Apply backward propogation
+
+        Args:
+            dvalues (numpy.ndarray) : inputs from previous later in backward prop
+        
+        Notes:
+            None
+        """
+        self.dinputs = dvalues.copy()
+        self.dinputs[self.inputs <= 0] = 0
 
 
-from os import XATTR_REPLACE
-import numpy as np
-
-np.random.seed(0)  # everytime random result will be same based on seed
-
-X = np.array([
-    [1.0, 2.0, 3, 2.5],
-    [2.0, 5.0, 1.0, 2.0],
-    [-1.5, 2.7, 3.3, -0.8]
-])  # (3,4) (number of examples, number of features)
+        
 
 
-def ReLU(x):
+class Activation_Softmax:
     """
     """
-    output = []
 
-    for i in x:
-        if i<0:
-            output.append(0)
-        else:
-            output.append(x)
+    def forward(self, inputs):
+        """Forward propogation calculation
 
-    return output 
+        Args:
+            inputs (numpy.ndarray) : input matrix
 
-def sigmoid(x):
-    """
-    """
-    output = []
+        Notes:
+            TODO
+        """
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilites = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilites
 
-    for i in x:
-        output.append(1/(1 + np.exp(-i)))
+    def backward(self, dvalues):
+        """backward pass
 
-    return output
+        Args:
+            dvalues (numpy.ndarray) : gradient values
 
-def stepwise(x):
-    """
-    """
-    output = []
-    for i in x:
-        if i <= 0:
-            output.append(0)
-        else:
-            output.append(1)
-
-
-def softmax(x):
-    """
-    """
-    exp_values = np.exp(x)
-
-    norm_values = exp_values  / np.sum(exp_values,axis=1,keepdims=True)
-
-    return norm_values
-
+        Notes:
+            None
+        """
+        pass
+        
