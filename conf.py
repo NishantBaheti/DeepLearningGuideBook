ad5783e916c0d7e17e4a056567d41986dc0b9163
Nishant Baheti <nishantbaheti.it19@gmail.com>
2024-01-03 23:36:26 +0530

    doc update

--- conf.py
+++ conf.py
@@ -60,3 +60,10 @@ html_theme = 'sphinx_book_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# The name for this set of Sphinx documents.  If None, it defaults to
+# "<project> v<release> documentation".
+html_title = "Deep Learning Guide Book"
+
+# A shorter title for the navigation bar.  Default is the same as html_title.
+html_short_title = "DL Guide Book"
