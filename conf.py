6ea43943b9ab4055d0d78c474afcd0dbe406bb31
Nishant Baheti <nishantbaheti.it19@gmail.com>
2024-12-22 13:57:59 +0000

    changing theme

--- conf.py
+++ conf.py
@@ -54,7 +54,11 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store','_build', '**.ipynb_check
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+# html_theme = 'sphinx_book_theme'
+
+import sphinx_theme_pd
+html_theme = 'sphinx_theme_pd'
+html_theme_path = [sphinx_theme_pd.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
